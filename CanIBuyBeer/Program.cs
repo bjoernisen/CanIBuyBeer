9dd8180d8a7b5945348836071962effe0b39d219
bjoernisen <b.lindebeerg@hotmail.com>
2020-09-01 10:31:27 +0200

    Add project files.

---
+++ CanIBuyBeer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// om ålder > 17 OCH location == "krogen" OCH om leg == "j" OCH om promille < 1.0 JA
+// om ålder > 19 och location == "systemet" OCH om leg == "j" OCH om promille < 1.0 JA
+
+
+namespace CanIBuyBeer
+{
+    class Program
+    {
+        static bool CanIBuyBeer(int age, string location, string leg, decimal promille);
+        {
+            if (leg!="j")
+                return false;
+            if (promille > 1.0m)
+	            return false
+            if (age > 17 && location =="krogen")
+                return true;
+            if (age > 19 && location =="systemet")
+	            return true; 
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Hur gammal är du?: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Location, systemet eller krogen?: ");
+            string location = Console.ReadLine().ToLower();
+            Console.Write("Har du leg med dig? (J/N): ");
+            string leg = Console.ReadLine().ToLower();
+            Console.Write("Vilken promillehalt har du?: ");
+            decimal promille = Convert.ToDecimal(Console.ReadLine());
+
+            CanIBuyBeer(); 
+            
+            if (canIBuyBeer)
+            {
+                Console.WriteLine("Du får köpa öl");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte köpa öl");
+            }
+            
+        }
+    }
+}
